f8eb63c3a08ad8cf1b3e9f611b9b40755600b05d
31Vector31 <lutaiyura@gmail.com>
2022-05-07 23:33:13 +0300

    add about-us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -17,9 +17,64 @@
 </head>
 <body>
 @include("parts.header")
-<br><br><br><br>
-О нас
-
+<br><br><br>
+<div class="container">
+    <hr>
+    <div class="py-3 text-center">
+        <img class="d-block mx-auto mb-4" src="{{env("APP_URL")}}/assets/img/Screenshot_6.png" alt="" width="1140" height="663">
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col"><h2 class="heading"></h2>
+            <ul>
+                <img class="d-block mx-auto mb-4" src="{{env("APP_URL")}}/assets/img/Screenshot_9.jpg" alt="" width="300" height="200">
+            </ul>
+        </div>
+        <div class="col"><h2 class="heading">Что мы делаем?</h2>
+            <ul>
+                <li><i class="ri-check-double-line"></i>Удобная площадка,
+                    где каждый в краткий срок может найти исполнителя для заданий, которые не может или не желает
+                    выполнять сами.
+                    Таким образом, с одной стороны сервис освобождает каждого человека от ненужных хлопот, а с другой -
+                    дает другим людям возможность заработать.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col"><h2 class="heading">Как мы делаем?</h2>
+            <ul>
+                <li><i class="ri-check-double-line"></i>Для создания этого проекта было использовано самые последние
+                    технологии в сфере Интернета.
+                    Поэтому Вы имеете возможность выбирать задания поблизости благодаря интерактивной карте, проверять
+                    изменения статусов заданий с помощью мобильных устройств.
+                </li>
+            </ul>
+        </div>
+        <div class="col"><h2 class="heading"></h2>
+            <ul>
+                <img class="d-block mx-auto mb-4" src="{{env("APP_URL")}}/assets/img/Screenshot_8.png" alt="" width="222" height="222">
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col"><h2 class="heading"></h2>
+            <ul>
+                <img class="d-block mx-auto mb-4" src="{{env("APP_URL")}}/assets/img/Screenshot_8.jpg" alt="" width="419" height="203">
+            </ul>
+        </div>
+        <div class="col"><h2 class="heading">Зачем мы это делаем?</h2>
+            <ul>
+                <li><i class="ri-check-double-line"></i>Ответ простой - мы сами нуждаемся в таком сервисе и регулярно им
+                    пользуемся. Мы постоянно развиваем этот проект для того,
+                    чтобы он всегда был надежным партнером как для простых людей, так и для бизнеса. Наша главная цель -
+                    помочь Вам быстро и удобно найти работника для любой задачи.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+</div>
 @include("parts.footer")
 </body>
 </html>
