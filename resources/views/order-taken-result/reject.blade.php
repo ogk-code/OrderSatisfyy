ef0bb6f43713023bb33aabb24ffca973f563dfd6
31Vector31 <lutaiyura@gmail.com>
2022-05-30 01:28:27 +0300

    confirm + reject шаблоны

---
+++ resources/views/order-taken-result/reject.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{env("APP_URL")}}/assets/img/favicon.png">
+    <title>Специалисту отказано</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/animate.min.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/bootstrap.min.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/boxicons.min.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/glightbox.min.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/remixicon.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/swiper-bundle.min.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/index.css">
+    <link rel="stylesheet" href="{{env("APP_URL")}}/assets/style/header.css">
+</head>
+<body>
+@include("parts.header")
+<div class="container">
+    <h1 style="margin: 20px">Специалисту отказано!</h1>
+</div>
+@include("parts.footer")
+</body>
+</html>
